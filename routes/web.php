9bf1666b7db7e1d29fdeb1bf6b67fa6b6109fc58
Zahwa Ihab <zahwa1703409@miuegypt.edu.eg>
2023-06-21 10:30:39 +0200

    adding styles

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\FrontEnd\FrontController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +29,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('play',[FrontController::class,'index']);
+
 });
 Route::post('user/logout/form',[ProfileController::class,'userLogout'])->name('user.logout.form');
 
