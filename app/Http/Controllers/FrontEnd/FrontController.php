2f2be271ca5dd18d934b9e9eb8fb7bba81d76c35
Zahwa Ihab <zahwa1703409@miuegypt.edu.eg>
2023-06-21 10:44:58 +0200

    customizing the style

--- app/Http/Controllers/FrontEnd/FrontController.php
+++ app/Http/Controllers/FrontEnd/FrontController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\FrontEnd;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         return view('frontend.index');
     }
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -6,17 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <!-- Boxicons CSS -->
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"/>
-    <title>Side Navigation Bar in HTML CSS JavaScript</title>
-    {{--    <link rel="stylesheet" href="style.css" />  --}}
+    <title>Drive-Thru</title>
     <link rel="stylesheet" href="{{asset('frontend/css/custom/style.css')}}"/>
 </head>
-<body>
+<body onload=display_ct();>
 <!-- navbar -->
 <nav class="navbar">
     <div class="logo_item">
         <!-- Sidebar Open / Close -->
         <div class="expand_sidebar" id="sidebarOpen">
-            {{--            <img class="" src="images/e422b1d1-e9e7-4619-886c-869091f6116a.png" alt=""> --}}
             <img class="" src="{{asset('images/dashboard/e422b1d1-e9e7-4619-886c-869091f6116a.png')}}" alt="">
         </div>
         <div class="collapse_sidebar">
@@ -29,8 +27,10 @@
     </div>
 
     <div class="data-time">
-        <p class="time">12:15:30 PM</p>
-        <p class="date">MAY 18 THURSDAY</p>
+        <p class="time" id='ct6'></p>
+        <p class="date">
+            {{ ucwords(Carbon\Carbon::now()->format('F d l')) }}
+        </p>
     </div>
 
 </nav>
@@ -45,9 +45,7 @@
             <li class="item">
                 <a href="#" class="nav_link">
                 <span class="navlink_icon">
-{{--                    <img src="images/o.png" alt="">--}}
-                                        <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
-
+                    <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
                 </span>
                     <span class="navlink">Average Serving Time</span>
                 </a>
@@ -55,7 +53,6 @@
             <li class="item">
                 <a href="#" class="nav_link">
                 <span class="navlink_icon">
-                    {{-- <img src="images/o.png" alt="">    --}}
                     <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
                 </span>
                     <span class="navlink">Count Of Cars Per Day</span>
@@ -64,7 +61,6 @@
             <li class="item">
                 <a href="#" class="nav_link">
                 <span class="navlink_icon">
-{{--                    <img src="images/o.png" alt="">--}}
                     <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
                 </span>
                     <span class="navlink">Count Of Cars Per Hour</span>
@@ -73,9 +69,7 @@
             <li class="item">
                 <a href="#" class="nav_link">
                 <span class="navlink_icon">
-{{--                    <img src="images/o.png" alt="">--}}
-                                        <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
-
+                    <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
                 </span>
                     <span class="navlink">Average Waiting Time</span>
                 </a>
@@ -83,32 +77,28 @@
             <li class="item">
                 <a href="#" class="nav_link">
                 <span class="navlink_icon">
-{{--                    <img src="images/o.png" alt="">--}}
-                                        <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
-
+                    <img class="" src="{{asset('images/dashboard/o.png')}}" alt="">
                 </span>
                     <span class="navlink">Total Served Cars</span>
                 </a>
             </li>
 
 
-            <div class="bottom_content">
+            <div class="bottom_content" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                 <div class="bottom">
-                    {{--                    <img src="images/Logout.png" alt="">--}}
                     <img class="" src="{{asset('images/dashboard/Logout.png')}}" alt="">
-
                     <span> Logout</span>
                 </div>
             </div>
+
+            <form class="d-none" id="logout-form" action="{{route('user.logout.form')}}" method="post">
+                @csrf
+            </form>
+
     </div>
 </nav>
 
 <div class="main">
-    {{--    <img class="left-arrow" src="images/0a3b996d-fc85-405b-86f4-31f3e1641813.png" alt="">--}}
-    {{--    <img class="right-arrow" src="images/4dc5a9b3-2f19-4f07-bb91-d47ba4c26760.png" alt="">--}}
-    {{--    <img class="block-1" src="images/7a440a85-5b42-419d-9ccc-fb39f31b57d5.png" alt="">--}}
-    {{--    <img class="car-1" src="images/42cd8f47-ae12-474b-933b-40ad4b367db4.png" alt="">--}}
-
     <!-- START CASHIER IMAGES -->
     <img class="left-arrow" src="{{asset('images/dashboard/0a3b996d-fc85-405b-86f4-31f3e1641813.png')}}" alt="">
     <img class="right-arrow" src="{{asset('images/dashboard/4dc5a9b3-2f19-4f07-bb91-d47ba4c26760.png')}}" alt="">
@@ -185,7 +175,42 @@
     </div>
 </div>
 <!-- JavaScript -->
-{{--<script src="index.js"></script>--}}
-<script src="{{asset('frontend/js/custom/index.js')}}" > </script>
+<script src="{{asset('frontend/js/custom/index.js')}}"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous">
+</script>
+
+<script>
+    function display_ct6() {
+        var x = new Date()
+        var ampm = x.getHours() >= 12 ? ' PM' : ' AM';
+        hours = x.getHours() % 12;
+        hours = hours ? hours : 12;
+        hours = hours - 1;
+        var x1 = x.getMonth() + 1 + "/" + x.getDate() + "/" + x.getFullYear();
+        // x1 = x1 + " - " +  hours + ":" +  x.getMinutes() + ":" +  x.getSeconds() + ":" + ampm;
+        // x1 = hours + ":" + x.getMinutes() + ":" + x.getSeconds() + ":" + ampm;
+        x1 = hours + ":" + x.getMinutes() + ":" + x.getSeconds() + ampm;
+        document.getElementById('ct6').innerHTML = x1;
+        display_c6();
+    }
+
+    function display_c6() {
+        var refresh = 1000; // Refresh rate in milli seconds
+        mytime = setTimeout('display_ct6()', refresh)
+    }
+    display_c6();
+</script>
+
+<script>
+    $(window).on('load', function () {
+        if (feather) {
+            feather.replace({
+                width: 14,
+                height: 14
+            });
+        }
+    })
+</script>
 </body>
 </html>
