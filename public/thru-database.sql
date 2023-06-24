a2318e32a5f8f787e61126f419812007a798e36f
Ahmed-Abdelrhim <aabdelrhim974@gmail.com>
2023-06-24 02:48:35 +0200

    database in the public directory

--- public/thru-database.sql
+++ public/thru-database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 21, 2023 at 04:16 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: Jun 24, 2023 at 02:46 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `login_at` datetime NOT NULL,
+  `logout_at` datetime DEFAULT NULL,
+  `total` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`id`, `login_at`, `logout_at`, `total`) VALUES
+(1, '2023-06-23 17:55:16', '2023-06-23 18:12:55', 16),
+(6, '2023-06-23 18:13:57', '2023-06-23 18:21:35', 15),
+(7, '2023-06-23 18:14:58', '2023-06-23 18:21:39', 5),
+(8, '2023-06-23 18:14:58', '2023-06-23 18:21:48', 21),
+(9, '2023-06-23 18:16:59', '2023-06-23 18:21:52', 10),
+(15, '2023-06-23 19:43:03', '2023-06-23 19:43:05', 17),
+(16, '2023-06-23 19:43:04', '2023-06-23 19:43:09', 19),
+(17, '2023-06-23 19:53:39', '2023-06-23 19:53:43', 10),
+(18, '2023-06-23 19:53:40', '2023-06-23 19:53:47', 18),
+(19, '2023-06-23 19:53:41', '2023-06-23 19:53:52', 7),
+(20, '2023-06-24 00:21:42', '2023-06-24 00:21:45', 5),
+(21, '2023-06-24 00:21:43', '2023-06-24 00:21:48', 4),
+(22, '2023-06-24 00:30:24', '2023-06-24 00:30:26', 20),
+(23, '2023-06-24 00:30:40', '2023-06-24 00:30:42', 15),
+(24, '2023-06-24 00:32:08', '2023-06-24 00:32:09', 16),
+(25, '2023-06-24 00:36:09', '2023-06-24 00:36:12', 12),
+(26, '2023-06-24 00:43:38', '2023-06-24 00:43:48', 12),
+(29, '2023-06-24 00:45:20', '2023-06-24 00:45:25', 15),
+(30, '2023-06-24 00:45:41', '2023-06-24 00:45:45', 0),
+(31, '2023-06-24 00:45:42', '2023-06-24 00:45:52', 0),
+(32, '2023-06-24 00:45:43', '2023-06-24 00:45:56', 0),
+(33, '2023-06-24 00:45:49', '2023-06-24 00:46:00', 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `failed_jobs`
 --
 
 CREATE TABLE `failed_jobs` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) NOT NULL,
-  `connection` text NOT NULL,
-  `queue` text NOT NULL,
-  `payload` longtext NOT NULL,
-  `exception` longtext NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -45,7 +86,7 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `migrations` (
   `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -58,7 +99,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2023_06_21_085942_create_permission_tables', 2);
+(5, '2023_06_21_085942_create_permission_tables', 2),
+(6, '2023_06_23_154658_create_cars_table', 2);
 
 -- --------------------------------------------------------
 
@@ -68,7 +110,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 
 CREATE TABLE `model_has_permissions` (
   `permission_id` bigint(20) UNSIGNED NOT NULL,
-  `model_type` varchar(255) NOT NULL,
+  `model_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `model_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -80,7 +122,7 @@ CREATE TABLE `model_has_permissions` (
 
 CREATE TABLE `model_has_roles` (
   `role_id` bigint(20) UNSIGNED NOT NULL,
-  `model_type` varchar(255) NOT NULL,
+  `model_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `model_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -91,17 +133,10 @@ CREATE TABLE `model_has_roles` (
 --
 
 CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `password_reset_tokens`
---
-
-INSERT INTO `password_reset_tokens` (`email`, `token`, `created_at`) VALUES
-('aabdelrhim974@gmail.com', '$2y$10$sGPNAESz/Pk9bm11mpCbmOWoWyL.niH8igXpEjsFbUU.QVkfUmHnu', '2023-06-13 06:30:18');
 
 -- --------------------------------------------------------
 
@@ -111,32 +146,11 @@ INSERT INTO `password_reset_tokens` (`email`, `token`, `created_at`) VALUES
 
 CREATE TABLE `permissions` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `guard_name` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guard_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `permissions`
---
-
-INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'user', 'web', '2023-06-21 09:21:43', NULL),
-(2, 'user_create', 'web', '2023-06-21 09:21:44', NULL),
-(3, 'user_edit', 'web', '2023-06-21 09:21:45', NULL),
-(4, 'user_show', 'web', '2023-06-21 09:21:46', NULL),
-(5, 'user_delete', 'web', '2023-06-21 09:35:55', NULL),
-(6, 'operator', 'web', '2023-06-21 09:52:09', NULL),
-(7, 'operator_create', 'web', '2023-06-21 09:52:12', NULL),
-(8, 'operator_edit', 'web', '2023-06-21 09:52:14', NULL),
-(9, 'operator_show', 'web', '2023-06-21 09:52:16', NULL),
-(10, 'operator_delete', 'web', '2023-06-21 09:52:20', NULL),
-(11, 'role', 'web', '2023-06-21 09:55:49', NULL),
-(12, 'role_create', 'web', '2023-06-21 09:55:51', NULL),
-(13, 'role_edit', 'web', '2023-06-21 09:55:54', NULL),
-(14, 'role_show', 'web', '2023-06-21 09:55:56', NULL),
-(15, 'role_delete', 'web', '2023-06-21 09:55:58', NULL);
 
 -- --------------------------------------------------------
 
@@ -146,11 +160,11 @@ INSERT INTO `permissions` (`id`, `name`, `guard_name`, `created_at`, `updated_at
 
 CREATE TABLE `personal_access_tokens` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tokenable_id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `token` varchar(64) NOT NULL,
-  `abilities` text DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -165,22 +179,11 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `roles` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `guard_name` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guard_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `roles`
---
-
-INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'web', '2023-06-21 09:16:42', NULL),
-(2, 'User', 'web', '2023-06-21 09:51:36', NULL),
-(3, 'Operator', 'web', '2023-06-21 09:51:37', NULL),
-(4, 'Roles', 'web', '2023-06-21 09:55:11', NULL),
-(5, 'Custom Role', 'web', '2023-06-21 09:58:10', '2023-06-21 09:58:10');
 
 -- --------------------------------------------------------
 
@@ -193,42 +196,6 @@ CREATE TABLE `role_has_permissions` (
   `role_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `role_has_permissions`
---
-
-INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
-(1, 1),
-(1, 4),
-(2, 1),
-(2, 4),
-(3, 1),
-(3, 4),
-(4, 1),
-(4, 4),
-(5, 1),
-(5, 4),
-(6, 1),
-(6, 4),
-(7, 1),
-(7, 4),
-(8, 1),
-(8, 4),
-(9, 1),
-(9, 4),
-(10, 1),
-(10, 4),
-(11, 1),
-(11, 4),
-(12, 1),
-(12, 4),
-(13, 1),
-(13, 4),
-(14, 1),
-(14, 4),
-(15, 1),
-(15, 4);
-
 -- --------------------------------------------------------
 
 --
@@ -237,12 +204,12 @@ INSERT INTO `role_has_permissions` (`permission_id`, `role_id`) VALUES
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` tinyint(1) NOT NULL DEFAULT 0,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `remember_token` varchar(100) DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -252,11 +219,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Ahmed Abdelrhim', 'aabdelrhim974@gmail.com', 1, NULL, '$2y$10$GziC8.2NzybOH5CfFVxCDOMyg9iuhcunf68hvXYQf0CstLK6GGsAW', NULL, '2023-06-11 12:43:01', '2023-06-11 12:43:02');
+(1, 'Ahmed Abdelrhim', 'aabdelrhim974@gmail.com', 1, NULL, '$2y$10$GziC8.2NzybOH5CfFVxCDOMyg9iuhcunf68hvXYQf0CstLK6GGsAW', NULL, '2023-06-11 12:43:01', '2023-06-11 12:43:02'),
+(2, 'Abanoub Rizk', 'acbnoub.rizk@qudra-tech.com', 0, NULL, '$2y$10$SBNOmvwRhTiISopzmu9S1.OZS5aU.4PFLfEk9Kw3vF4Jzv1gtKlT.', NULL, '2023-06-23 17:08:52', '2023-06-23 17:08:52'),
+(3, 'Ahmed El-Hawary', 'ahmed.hwary@qudra-tech.com', 1, NULL, '$2y$10$OUa0mS3.wpEDRMgOyFbDceE44xrEX/UiSdxocGDTZyjfINstUOOBC', NULL, '2023-06-23 17:10:47', '2023-06-23 17:10:47');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -332,6 +307,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -341,13 +322,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `permissions`
 --
 ALTER TABLE `permissions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -359,13 +340,13 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
